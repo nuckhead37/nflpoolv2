33558594103c4f35739c6d1dafdbe2acc3c65a1c
nuckhead37 <nuckhead@elfstar.co.uk>
2026-06-06 12:12:46 +0100

    initial controllers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [HomeController::class, 'home']);
