f547ec9018ce25e441bbf6cbfed1cadc3b5bae48
nuckhead37 <nuckhead@elfstar.co.uk>
2026-07-22 19:47:57 +0100

    Improved mobile styling

--- resources/views/history/history_year.blade.php
+++ resources/views/history/history_year.blade.php
@@ -13,10 +13,10 @@
             <div class="cell header">Week</div>
             <div class="cell header">Player</div>
             <div class="cell header">Score</div>
-            <div class="cell header">Wins</div>
-            <div class="cell header">Tied</div>
             <div class="cell header">Leader</div>
             <div class="cell header">Total</div>
+            <div class="cell header">Wins</div>
+            <div class="cell header">Tied</div>
             @foreach ($weekResults as $key => $weeks)
                 @foreach ($weeks as $weekKey => $week)
                     <!-- Column 1 -->
@@ -25,18 +25,18 @@
                     <!-- Columns 2-7 (top row) -->
                     <div class="cell leader">{{ $week['users'][0]['name'] }}</div>
                     <div class="cell leader">{{ $week['users'][0]['points'] }}</div>
-                    <div class="cell leader">{{ $week['totals'][0]['wins'] }}</div>
-                    <div class="cell leader">{{ $week['totals'][0]['tied'] }}</div>
                     <div class="cell {{ $week['totals'][0]['class'] }}">{{ $week['totals'][0]['name'] }}</div>
                     <div class="cell {{ $week['totals'][0]['class'] }}">{{ $week['totals'][0]['total'] }}</div>
+                    <div class="cell leader">{{ $week['totals'][0]['wins'] }}</div>
+                    <div class="cell leader">{{ $week['totals'][0]['tied'] }}</div>
 
                     <!-- Columns 2-7 (bottom row) -->
-                    <div class="cell">{{ $week['users'][1]['name'] }}</div>
-                    <div class="cell">{{ $week['users'][1]['points'] }}</div>
-                    <div class="cell">{{ $week['totals'][1]['wins'] }}</div>
-                    <div class="cell">{{ $week['totals'][1]['tied'] }}</div>
+                    <div class="cell {{ $week['totals'][1]['tied'] === 1 ? ' leader ' : '' }} ">{{ $week['users'][1]['name'] }}</div>
+                    <div class="cell {{ $week['totals'][1]['tied'] === 1 ? ' leader ' : '' }}">{{ $week['users'][1]['points'] }}</div>
                     <div class="cell {{ $week['totals'][1]['class'] }}">{{ $week['totals'][1]['name'] }}</div>
                     <div class="cell {{ $week['totals'][1]['class'] }}">{{ $week['totals'][1]['total'] }}</div>
+                    <div class="cell">{{ $week['totals'][1]['wins'] }}</div>
+                    <div class="cell">{{ $week['totals'][1]['tied'] }}</div>
                 @endforeach
             @endforeach
         </div>
