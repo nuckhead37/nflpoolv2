6bcb4b1e90784c1134a1aefb097bd76eac9b184b
nuckhead37 <nuckhead@elfstar.co.uk>
2026-08-08 18:33:21 +0100

    corrected admin styling

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -6,7 +6,7 @@
     <div class="button-container">
         <a href="/enter-results" class="button-link {{ $inputResultDisabled }}">Input Results</a>
         <a href="/recalculate-results" class="button-link {{ $recalculateResultDisabled }}">Recalculate Results</a>
-        <a href="/update-picks" class="button-link {{ $updatePicksDisabled }}"">Update Picks</a>
+        <a href="/update-picks" class="button-link {{ $updatePicksDisabled }}">Update Picks</a>
         <a href="/create-season" class="button-link {{ $createNewSeasonDisabled }}">Create New Season</a>
         <a href="/edit-settings" class="button-link">Edit Settings</a>
         <a href="/manage-users" class="button-link">Manage Users</a>
